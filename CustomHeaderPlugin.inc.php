04c7cee122135a719152f0f60ccbfded5c5ba7f5
Alec Smecher <alec@smecher.bc.ca>
2018-10-01 16:59:21 -0700

    Remove unused function

--- CustomHeaderPlugin.inc.php
+++ CustomHeaderPlugin.inc.php
@@ -83,7 +83,6 @@ class CustomHeaderPlugin extends GenericPlugin {
 
 				AppLocale::requireComponents(LOCALE_COMPONENT_APP_COMMON,  LOCALE_COMPONENT_PKP_MANAGER);
 				$templateMgr = TemplateManager::getManager($request);
-				$templateMgr->registerPlugin('function', 'plugin_url', array($this->plugin, 'smartyPluginUrl'));
 
 				$this->import('CustomHeaderSettingsForm');
 				$form = new CustomHeaderSettingsForm($this, $context?$context->getId():CONTEXT_ID_NONE);
