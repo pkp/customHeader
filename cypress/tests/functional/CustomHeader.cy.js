43ae9d4210bda8a05fd30454aa9c258e9b983334
Touhidur Rahman <abir.ruet@gmail.com>
2023-01-16 18:01:36 +0600

    pkp/pkp-lib#8523 cypress test and travis config update

--- cypress/tests/functional/CustomHeader.cy.js
+++ cypress/tests/functional/CustomHeader.cy.js
@@ -1,5 +1,5 @@
 /**
- * @file cypress/tests/functional/CustomHeader.spec.js
+ * @file cypress/tests/functional/CustomHeader.cy.js
  *
  * Copyright (c) 2014-2021 Simon Fraser University
  * Copyright (c) 2000-2021 John Willinsky
