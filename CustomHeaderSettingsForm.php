cbd452a543786d1de2f2095a582bd69694acf91c
Alec Smecher <alec@smecher.bc.ca>
2023-03-31 16:36:28 -0700

    Prep for 3.4.0 compatibility release

--- CustomHeaderSettingsForm.php
+++ CustomHeaderSettingsForm.php
@@ -3,8 +3,8 @@
 /**
  * @file CustomHeaderSettingsForm.php
  *
- * Copyright (c) 2013-2021 Simon Fraser University
- * Copyright (c) 2003-2021 John Willinsky
+ * Copyright (c) 2013-2023 Simon Fraser University
+ * Copyright (c) 2003-2023 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
  *
  * @brief Form for managers to modify custom header plugin settings
