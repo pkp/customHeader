0d842281716c02fdfd77bd34b96e3cad898f9032
Kaitlin Newson <kaitlin.newson@gmail.com>
2025-07-24 17:59:34 -0300

    Update cypress test

--- cypress/tests/functional/CustomHeader.cy.js
+++ cypress/tests/functional/CustomHeader.cy.js
@@ -11,16 +11,18 @@ describe('Custom Header plugin tests', function() {
 	it('Creates and exercises a custom header', function() {
 		cy.login('admin', 'admin', 'publicknowledge');
 
-		cy.get('.app__nav a').contains('Website').click();
+		cy.get('nav').contains('Settings').click();
+		// Ensure submenu item click despite animation
+		cy.get('nav').contains('Website').click({ force: true });
 		cy.get('button[id="plugins-button"]').click();
 
 		// Find and enable the plugin
 		cy.get('input[id^="select-cell-customheaderplugin-enabled"]').click();
 		cy.get('div:contains(\'The plugin "Custom Header Plugin" has been enabled.\')');
-		cy.waitJQuery();
+		cy.reload();
 
-		cy.get('tr[id*="customheaderplugin"] a.show_extras').click();
-		cy.get('a[id*="customheaderplugin-settings"]').click();
+		cy.get('tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-customheaderplugin"] a.show_extras').click();
+		cy.get('a[id^="component-grid-settings-plugins-settingsplugingrid-category-generic-row-customheaderplugin-settings-button"').click();
 		cy.waitJQuery(2000); // Wait for form to settle
 		cy.get('textarea[id^="headerContent-"]').type('<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>', {delay: 0});
 		cy.get('textarea[id^="footerContent-"]').type('<a class="twitter-timeline" href="https://twitter.com/pkp?ref_src=twsrc%5Etfw">Tweets by pkp</a>', {delay: 0});
